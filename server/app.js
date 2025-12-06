03a838b87865678002873a02b3a3a5982d377cf3
AB45699 <b.anam8541@gmail.com>
2025-12-06 19:14:57 +0000

    controller file for error handlers

--- server/app.js
+++ server/app.js
@@ -1,12 +1,11 @@
 const express = require("express"); 
 const {getAllCases} = require("./controllers/cases.js");
+const {handlePathNotFound} = require("./controllers/errors.js");
 
 const app = express(); 
 
 app.get("/api/cases", getAllCases);
 
-app.all("/*path", (req, res, next)=>{
-    res.status(404).send({msg: "Path not found"})
-})
+app.all("/*path", handlePathNotFound);
 
 module.exports = app;

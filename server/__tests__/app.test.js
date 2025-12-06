a3568b4de50e65e596936726f831ebff35089ebf
AB45699 <b.anam8541@gmail.com>
2025-12-06 19:03:55 +0000

    testing shape and order of returned cases data

--- server/__tests__/app.test.js
+++ server/__tests__/app.test.js
@@ -15,6 +15,36 @@ describe("app", ()=> {
             const {body} = await request(app).get("/api/cases"); 
 
             expect(Array.isArray(body.cases)).toBe(true); 
+        });
+        test("array length is 2, to reflect the number of test cases data", async ()=> {
+            const {body} = await request(app).get("/api/cases"); 
+
+            expect(body.cases.length).toBe(2);
+        });
+        test("each test case item is an object", async ()=> {
+            const {body} = await request(app).get("/api/cases"); 
+
+            body.cases.forEach((testCase)=>{
+                expect(typeof testCase).toBe("object"); 
+                expect(Array.isArray(testCase)).toBe(false); 
+            })
+        }); 
+        test("each test case item has keys of case_id, case_number, case_title, case_description, case_status, and due", async () => {
+            const {body} = await request(app).get("/api/cases"); 
+
+            body.cases.forEach((testCase)=>{ 
+                expect(testCase).toHaveProperty("case_id");
+                expect(testCase).toHaveProperty("case_number");
+                expect(testCase).toHaveProperty("case_title");
+                expect(testCase).toHaveProperty("case_description");
+                expect(testCase).toHaveProperty("case_status");
+                expect(testCase).toHaveProperty("due");
+            })
+        });
+        test("the test case items are in descending order of creation (i.e. case_id)", async ()=>{
+            const {body} = await request(app).get("/api/cases"); 
+
+            expect(body.cases).toBeSortedBy("case_id", {descending: true});
         })
     })
 })
--- server/models/cases.js
+++ server/models/cases.js
@@ -1,7 +1,7 @@
 const db = require("../database/connection.js");
 
 exports.fetchAllCases = async () => {
-    const {rows: cases} = await db.query(`SELECT * FROM cases;`);
+    const {rows: cases} = await db.query(`SELECT * FROM cases ORDER BY case_id DESC;`);
 
     return cases;
 }

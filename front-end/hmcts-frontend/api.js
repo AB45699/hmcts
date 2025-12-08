d746ae32954a19f09753df5c2239b220915a6647
AB45699 <b.anam8541@gmail.com>
2025-12-08 14:45:35 +0000

    refactoring

--- front-end/hmcts-frontend/api.js
+++ front-end/hmcts-frontend/api.js
@@ -6,8 +6,8 @@ export const fetchAllCases = async () => {
     return allCases
 };
 
-export const postCaseData = async (caseData) => {
-    const {data} = await axios.post("http://localhost:9090/api/cases", caseData);
+export const postCaseData = async (newCaseData) => {
+    const {data} = await axios.post("http://localhost:9090/api/cases", newCaseData);
    
     return data;
 }
